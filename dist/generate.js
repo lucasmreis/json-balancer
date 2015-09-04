8b74591d8b2a6927492ba3bca83c93bd6c146280
Lucas Reis <lucas.reis@b2wdigital.com>
2015-09-04 17:57:01 -0300

    v0.2.0

--- dist/generate.js
+++ dist/generate.js
@@ -1,9 +1,57 @@
-"use strict";
+'use strict';
 
-Object.defineProperty(exports, "__esModule", {
+Object.defineProperty(exports, '__esModule', {
   value: true
 });
+var defaultFromOptions = function defaultFromOptions(options) {
+  return options.filter(function (o) {
+    return o.defaultValue;
+  }).map(function (o) {
+    return o.value;
+  })[0];
+};
+
+exports.defaultFromOptions = defaultFromOptions;
+var defaultFromBalanced = function defaultFromBalanced(obj) {
+  return defaultFromOptions(obj.balance);
+};
+
+exports.defaultFromBalanced = defaultFromBalanced;
+var randomFromBalanced = function randomFromBalanced(obj) {
+  var options = obj.balance;
+  return 12345;
+};
+
+exports.randomFromBalanced = randomFromBalanced;
+// generates final object given transformation
+// example transformations:
+// - defaultFromBalanced
+// - randomFromBalanced
+var generateRecur = function generateRecur(fn, config, result) {
+  Object.keys(config).forEach(function (k) {
+    if (typeof config[k] !== 'object') {
+      result[k] = config[k];
+    } else if (config[k].hasOwnProperty('balance')) {
+      result[k] = fn(config[k]);
+    } else {
+      result[k] = {};
+      generateRecur(fn, config[k], result[k]);
+    }
+  });
+  return result;
+};
+
+exports.generateRecur = generateRecur;
+var generateDefault = function generateDefault(config) {
+  var result = {};
+  generateRecur(defaultFromBalanced, config, result);
+  return result;
+};
+
+exports.generateDefault = generateDefault;
 var generate = function generate(config) {
-  id: 1234;
+  var result = {};
+  generateRecur(randomFromBalanced, config, result);
+  return result;
 };
 exports.generate = generate;
--- dist/validate.js
+++ dist/validate.js
@@ -1,9 +1,123 @@
-"use strict";
+//
+// Example of a config object:
+//
+// {
+//   normal: 12345,
+//   weddingList: {
+//     balance: [
+//       {percent: 50, value: true, defaultValue: true},
+//       {percent: 50, value: false}
+//     ]
+//   }
+// }
 
-Object.defineProperty(exports, "__esModule", {
+'use strict';
+
+Object.defineProperty(exports, '__esModule', {
   value: true
 });
+var buildErrorObj = function buildErrorObj(msg, obj) {
+  return { message: msg, data: obj !== undefined ? obj : 'undefined-data' };
+};
+
+var is = function is(testType) {
+  return function (value) {
+    return typeof value === testType;
+  };
+};
+
+var validatePercent = function validatePercent(p) {
+  return !!p && is('number')(p) && p > 0 && p < 100;
+};
+exports.validatePercent = validatePercent;
+var validateValue = function validateValue(v) {
+  return !!v;
+};
+exports.validateValue = validateValue;
+var validateDefault = function validateDefault(d) {
+  return !d || !!d && d === true;
+};
+
+exports.validateDefault = validateDefault;
+// runs validaten fn and log errors
+var testValidation = function testValidation(errors, msg, fn, value) {
+  var validation = fn(value);
+  if (validation) {
+    return true;
+  } else {
+    errors.push(buildErrorObj(msg, value));
+    return false;
+  }
+};
+
+exports.testValidation = testValidation;
+// validates single option
+// object. Ex:
+// {percent: 50, value: true, defaultValue: true}
+var validateOption = function validateOption(errors, o) {
+  return testValidation(errors, 'Option is not an object', is('object'), o) && testValidation(errors, 'Percent is not valid', validatePercent, o.percent) && testValidation(errors, 'Option must have value', validateValue, o.value) && testValidation(errors, 'Default must only be true', validateDefault, o.defaultValue);
+};
+
+exports.validateOption = validateOption;
+var percentSum100 = function percentSum100(arr) {
+  return 100 === arr.map(function (o) {
+    return o.percent;
+  }).reduce(function (a, b) {
+    return a + b;
+  });
+};
+
+exports.percentSum100 = percentSum100;
+var hasDefault = function hasDefault(arr) {
+  return false || arr.map(function (o) {
+    return o.defaultValue;
+  }).reduce(function (a, b) {
+    return !!a || !!b;
+  });
+};
+
+exports.hasDefault = hasDefault;
+var validateIndividually = function validateIndividually(errors, arr) {
+  return true && arr.map(function (o) {
+    return validateOption(errors, o);
+  }).reduce(function (a, b) {
+    return !!a && !!b;
+  });
+};
+
+exports.validateIndividually = validateIndividually;
+// validates array of options
+var validateOptionArray = function validateOptionArray(errors, arr) {
+  return testValidation(errors, 'The options array must have at least two items', function (a) {
+    return a.length >= 2;
+  }, arr) && testValidation(errors, 'Sum of percent must be 100', percentSum100, arr) && testValidation(errors, 'At least one option must have defaultValue: true', hasDefault, arr) && validateIndividually(errors, arr);
+};
+
+exports.validateOptionArray = validateOptionArray;
+// validates property which can be a value,
+// an object or a balance object with an
+// array of options
+var validateProperty = function validateProperty(errors, prop) {
+  if (!is('object')(prop)) {
+    return true;
+  } else if (prop.hasOwnProperty('balance')) {
+    return validateOptionArray(errors, prop.balance);
+  } else {
+    return true && Object.keys(prop).map(function (k) {
+      return prop[k];
+    }).map(function (p) {
+      return validateProperty(errors, p);
+    }).reduce(function (a, b) {
+      return !!a && !!b;
+    });
+  }
+};
+
+exports.validateProperty = validateProperty;
+// final validate function
 var validate = function validate(config) {
-  return true;
+  var errors = [];
+  var ok = validateProperty(errors, config);
+  return ok ? true : errors;
 };
 exports.validate = validate;
